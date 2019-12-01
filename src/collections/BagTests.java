289c0ee6de0b0e765b3c3cbf406b95349fb4f2f9
Chris Emerson <chris.emerson@roledex.com>
2019-11-30 20:28:23 -0700

    Add isEmpty tests and nesting to Bag tests
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

--- src/collections/BagTests.java
+++ src/collections/BagTests.java
@@ -1,63 +1,88 @@
 package collections;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class BagTests {
-  @Test
-  void constructorDoesntThrow() {
-    new Bag<Integer>(1);
+  @Nested
+  class Constructor {
+    @Test
+    void doesntThrow() {
+      new Bag<Integer>(1);
+    }
+
+    @Test
+    void throwsForZeroInitialCapacity() {
+      assertThrows(
+          IllegalArgumentException.class,
+          () -> new Bag<Integer>(0)
+      );
+    }
+
+    @Test
+    void throwsForNegativeInitialCapacity() {
+      assertThrows(
+          IllegalArgumentException.class,
+          () -> new Bag<Integer>(-1)
+      );
+    }
+
+    @Test
+    void initialSizeIsZero() {
+      Bag bag = new Bag<Integer>(10);
+
+      assertEquals(0, bag.size());
+    }
   }
 
-  @Test
-  void constructorThrowsForZeroInitialCapacity() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> new Bag<Integer>(0)
-    );
+  @Nested
+  class Add {
+    @Test
+    void addDoesntThrow() {
+      Bag bag = new Bag<Integer>(10);
+
+      bag.add(1);
+    }
+
+    @Test
+    void addIncreasesSize() {
+      Bag bag = new Bag<Integer>(10);
+
+      bag.add(1);
+      assertEquals(1, bag.size());
+      bag.add(1);
+      assertEquals(2, bag.size());
+    }
+
+    @Test
+    void addWorksAboveInitialCapacity() {
+      Bag bag = new Bag<Integer>(1);
+
+      bag.add(1);
+      assertEquals(1, bag.size());
+      bag.add(1);
+      assertEquals(2, bag.size());
+    }
   }
 
-  @Test
-  void constructorThrowsForNegativeInitialCapacity() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> new Bag<Integer>(-1)
-    );
-  }
+  @Nested
+  class IsEmpty {
+    @Test
+    void returnsTrueWhenEmpty() {
+      Bag bag = new Bag<Integer>(1);
 
-  @Test
-  void initialSizeIsZero() {
-    Bag bag = new Bag<Integer>(10);
+      assertTrue(bag.isEmpty());
+    }
 
-    assertEquals(0, bag.size());
-  }
+    @Test
+    void returnsFalseWhenNotEmpty() {
+      Bag bag = new Bag<Integer>(1);
+      bag.add(0);
 
-  @Test
-  void addDoesntThrow() {
-    Bag bag = new Bag<Integer>(10);
-
-    bag.add(1);
-  }
-
-  @Test
-  void addIncreasesSize() {
-    Bag bag = new Bag<Integer>(10);
-
-    bag.add(1);
-    assertEquals(1, bag.size());
-    bag.add(1);
-    assertEquals(2, bag.size());
-  }
-
-  @Test
-  void addWorksAboveInitialCapacity() {
-    Bag bag = new Bag<Integer>(1);
-
-    bag.add(1);
-    assertEquals(1, bag.size());
-    bag.add(1);
-    assertEquals(2, bag.size());
+      assertFalse(bag.isEmpty());
+    }
   }
 }
