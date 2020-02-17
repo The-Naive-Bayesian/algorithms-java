a697911d065f25ddb0d48c5ec0a3fde214fee934
Chris Emerson <chris.emerson@roledex.com>
2020-02-16 22:04:25 -0700

    Create queue class
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

---
+++ src/collections/Queue.java
@@ -0,0 +1,19 @@
+package collections;
+
+public class Queue<T> {
+  void enqueue(T item) {
+
+  }
+
+  T dequeue() {
+    return null;
+  }
+
+  boolean isEmpty() {
+    return true;
+  }
+
+  int size() {
+    return 0;
+  }
+}
