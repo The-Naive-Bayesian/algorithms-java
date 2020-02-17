617caaa78e29187b70468a42de25bfa1bff69fc4
Chris Emerson <chris.emerson@roledex.com>
2020-02-16 22:06:07 -0700

    Create stack class
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

---
+++ src/collections/Stack.java
@@ -0,0 +1,19 @@
+package collections;
+
+public class Stack<T> {
+  void push() {
+
+  }
+
+  T pop() {
+    return null;
+  }
+
+  boolean isEmpty() {
+    return true;
+  }
+
+  int size() {
+    return 0;
+  }
+}
